a668f47001fd5baf39b37211cc1aa4e3c72925b8
Noah Tigner <noahzanetigner@gmail.com>
2023-12-16 16:26:04 -0700

    Initial Script

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import {
+	validateEnvVars,
+	parseEnvFile,
+	parseTemplateEnvVar,
+} from './validate.js';
+
+try {
+	// parse command line arguments
+	const args = process.argv.slice(2);
+
+	let expectedEnvVars = [];
+	let envFilePath = '.env';
+
+	for (let i = 0; i < args.length; i++) {
+		if (args[i] === '-l' || args[i] === '--list') {
+			expectedEnvVars = args[i + 1].split(',');
+			i++;
+		} else if (args[i] === '-t' || args[i] === '--template') {
+			expectedEnvVars = parseEnvFile(args[i + 1]).map(
+				parseTemplateEnvVar
+			);
+			i++;
+		} else if (args[i] === '-f' || args[i] === '--file') {
+			envFilePath = args[i + 1];
+			i++;
+		} else {
+			throw new Error(`Unknown argument: ${args[i]}`);
+		}
+	}
+
+	if (!expectedEnvVars || expectedEnvVars.length === 0) {
+		throw new Error(
+			'You must provide either a list of environment variables with -l or --list, or a template file with -t or --template.'
+		);
+	}
+
+	const receivedEnvVars = parseEnvFile(envFilePath);
+
+	// validate environment variables
+	validateEnvVars(expectedEnvVars, receivedEnvVars);
+} catch (error) {
+	console.error(error.message);
+	process.exit(1);
+}
---
+++ src/validate.js
@@ -0,0 +1,98 @@
+import fs from 'fs';
+import path from 'path';
+
+const ERR_SYMBOL = '\x1b[31m✖\x1b[0m';
+const OK_SYMBOL = '\x1b[32m✔\x1b[0m';
+const WARN_SYMBOL = '\x1b[33m⚠\x1b[0m';
+
+const OPTIONAL_SUFFIX = ' (optional)';
+
+/**
+ * Checks if an environment variable is defined and has a non-empty value in a given line.
+ *
+ * @param {string} envVar - The name of the environment variable to check.
+ * @param {string} line - The line from the .env file to check.
+ * @returns {boolean} - Returns true if the environment variable is defined and has a non-empty value, false otherwise.
+ */
+const validateEnvVar = (envVar, line) => {
+	const regex = new RegExp(
+		`^${envVar}=(\\$\\{[a-zA-Z0-9_-]+\\}|[a-zA-Z0-9_-]+)$`
+	);
+	return regex.test(line);
+};
+
+/**
+ * Validates that the specified environment variables are defined in the given .env file.
+ *
+ * @param {string[]} expectedEnvVars - A list of the names of the environment variables to check.
+ * @param {string[]} receivedEnvVars - A list of the names of the environment variables that were found in the .env file.
+ * @throws {Error} - If the .env file does not exist, or if any of the environment variables are missing or invalid.
+ * @returns {void}
+ */
+const validateEnvVars = (expectedEnvVars, receivedEnvVars) => {
+	const expectedSet = new Set(expectedEnvVars);
+	const missing = [];
+
+	for (const envVar of expectedSet) {
+		const isOptional = envVar.endsWith(OPTIONAL_SUFFIX);
+		const strippedEnvVar = envVar.replace(OPTIONAL_SUFFIX, '');
+
+		if (
+			!receivedEnvVars.find((line) =>
+				validateEnvVar(strippedEnvVar, line)
+			)
+		) {
+			if (isOptional) {
+				console.warn(`${WARN_SYMBOL} ${envVar}`);
+				continue;
+			}
+			missing.push(envVar);
+			console.error(`${ERR_SYMBOL} ${envVar}`);
+		} else {
+			console.log(`${OK_SYMBOL} ${envVar}`);
+		}
+	}
+
+	if (missing.length > 0) {
+		throw new Error(
+			`Missing or invalid environment variable${
+				missing.length === 1 ? '' : 's'
+			}: ${missing.join(', ')}`
+		);
+	}
+};
+
+/**
+ * Extracts the name of an environment variable from a given line; strips out any comments or whitespace.
+ *
+ * @param {string} line - The line from the .env file to check.
+ * @returns {string} - The name of the environment variable. " (optional)" is appended to the name if the variable is optional.
+ */
+const parseTemplateEnvVar = (line) => {
+	const isOptional = line.toLowerCase().includes('# optional');
+	const strippedLine = line.split('=')[0].split(' ')[0].split('#')[0];
+	return isOptional ? `${strippedLine}${OPTIONAL_SUFFIX}` : strippedLine;
+};
+
+/**
+ * Returns each line from a given .env file as an array of strings.
+ *
+ * @param {string} envFilePath - The path to the .env file to parse.
+ * @throws {Error} - If the .env file does not exist or cannot be read.
+ * @returns {string[]} - An array of strings, where each string represents a line from the .env file.
+ */
+const parseEnvFile = (envFilePath) => {
+	const absoluteEnvFilePath = path.resolve(process.cwd(), envFilePath);
+
+	if (!fs.existsSync(absoluteEnvFilePath)) {
+		throw new Error(`File not found: ${absoluteEnvFilePath}`);
+	}
+
+	const envFileContent = fs
+		.readFileSync(absoluteEnvFilePath, 'utf-8')
+		.split('\n');
+
+	return envFileContent;
+};
+
+export { validateEnvVars, validateEnvVar, parseTemplateEnvVar, parseEnvFile };
