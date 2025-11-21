b8225fc817af646866fea3bddaaefb535ec7b097
Noah Tigner <noahzanetigner@gmail.com>
2025-11-20 17:49:01 -0700

    Add plugins to eslint v9 config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'eslint/config';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import jestPlugin from 'eslint-plugin-jest';
+import importPlugin from 'eslint-plugin-import';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig(
+	// extend recommended configs
 	eslint.configs.recommended,
 	tseslint.configs.strictTypeChecked,
-	{
-		ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
-	},
+	eslintConfigPrettier,
+	// general ignore patterns
+	{ ignores: ['dist', 'node_modules', 'coverage'] },
+	// specify parser options for TypeScript
 	{
 		languageOptions: {
 			parserOptions: {
@@ -16,5 +21,18 @@ export default defineConfig(
 				},
 			},
 		},
+	},
+	// only apply TypeScript rules to TypeScript files
+	{
+		files: ['**/*.{ts,tsx}'],
+		extends: [
+			importPlugin.flatConfigs.recommended,
+			importPlugin.flatConfigs.typescript,
+		],
+	},
+	// only apply jest rules to test files
+	{
+		files: ['test/**'],
+		...jestPlugin.configs['flat/all'],
 	}
 );
