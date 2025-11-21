24722c559487fd91ef1206090ab9bcbe5874f676
Noah Tigner <46695778+noahtigner@users.noreply.github.com>
2025-11-20 18:01:28 -0700

    Migrate to eslint v9 (#184)
    * Migrate to eslint v9
    * Add plugins to eslint v9 config and introduce new rules
    * Temporarily set new eslint rules to `warn`

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import { defineConfig } from 'eslint/config';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import jestPlugin from 'eslint-plugin-jest';
+import importPlugin from 'eslint-plugin-import';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default defineConfig(
+	// extend recommended configs
+	eslint.configs.recommended,
+	tseslint.configs.strictTypeChecked,
+	eslintConfigPrettier,
+	// general ignore patterns
+	{ ignores: ['dist', 'node_modules', 'coverage'] },
+	// specify parser options for TypeScript
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: {
+					allowDefaultProject: ['*.js', '*.ts', '__tests__/*.ts'],
+				},
+			},
+		},
+	},
+	// only apply TypeScript rules to TypeScript files
+	{
+		files: ['**/*.{ts,tsx}'],
+		extends: [
+			importPlugin.flatConfigs.recommended,
+			importPlugin.flatConfigs.typescript,
+		],
+	},
+	// only apply jest rules to test files
+	{
+		files: ['test/**'],
+		...jestPlugin.configs['flat/all'],
+	},
+	// temporarily set some rules to warn level
+	{
+		rules: {
+			'@typescript-eslint/no-unnecessary-condition': 'warn',
+			'@typescript-eslint/no-base-to-string': 'warn',
+			'@typescript-eslint/no-duplicate-type-constituents': 'warn',
+			'@typescript-eslint/no-deprecated': 'warn',
+			'@typescript-eslint/no-confusing-void-expression': 'warn',
+			'@typescript-eslint/restrict-template-expressions': 'warn',
+		},
+	}
+);
